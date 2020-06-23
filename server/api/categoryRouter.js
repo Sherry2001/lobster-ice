85902679161ddb4cab8b4ad6ef259a3385b54f18
Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
2020-06-23 15:02:30 -0400

    create getCategoryItems router

--- server/api/categoryRouter.js
+++ server/api/categoryRouter.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 const Categories = require('../db/models/category')
-
+const Items = require('../db/models/item')
 const router = express()
 
 /**
@@ -29,5 +29,56 @@ router.get('/getCategories', (req, res) => {
     }
   });
 });
+
+/**
+ * Get all the items belonging in a specified category
+ * 
+ * req.body: {categoryId: mongoose.objectId}
+ * 
+ * response: [{title: String, 
+ *             items: [{
+ *               item object from db 
+ *             }]
+ *            }]
+ */
+router.get('/getCategoryItems', (req, res) => {
+  const categoryId = req.body.categoryId;
+  response = {}
+  Categories.findOne({_id: categoryId}, 'title items', (err, data) => {
+    if (err) {
+      throw err;
+    } 
+    if (data) {
+      return data
+    }
+  }).catch((err) => {
+    next(err);
+  }).then((data) => {
+    this.response.title = data.title;
+    itemObjects = [];
+    itemIds = data.items;
+    itemIds.forEach( async (itemId) => {
+      const itemObject = await getItemInfo(itemId);
+      itemObjects.push(itemObject); 
+    }) 
+    this.response.items = itemObjects;
+    res.json(this.response);
+  })
+});
+
+/**
+ * Helper function to return an item object given item id 
+ * @param  itemId 
+ */
+async function getItemInfo(itemId) {
+  Items.findById(itemId, (err, data) => {
+    if (err) {
+      throw new Error('error getting itemInfo: ', err);
+    } 
+    if(data){
+      return data;
+    }
+  })
+}
 
 module.exports = router;
