0013ec9b9d68abb5e499cecfacb7a4a9abdbb2ac
Sherry Shi <sherryshi@SherrysMacBook4.home>
2020-06-22 20:31:32 -0400

    create user schema

---
+++ server/db/models/user.js
@@ -0,0 +1,13 @@
+/**
+ * Schema for user
+ * 
+ */
+
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  email: {type: String, required: true},
+  username: {type: String},
+});
+
+module.exports = mongoose.model('users', userSchema);
