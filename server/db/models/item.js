655deb775b16aa212528417a7ecfdc9130b168c1
Sherry Shi <sherryshi@SherrysMacBook4.home>
2020-06-22 20:05:48 -0400

    create item mongoose schema

---
+++ server/db/models/item.js
@@ -0,0 +1,17 @@
+/**
+ * Schema for item
+ * 
+ */
+
+var mongoose = require('mongoose')
+
+var itemSchema = new mongoose.Schema({
+  sourceLink: {type: String, required: true},
+  highlight: {type: String, required: true},
+  userId: {type: mongoose.ObjectId, required: true},
+  note: {type: String},
+  placesId: {type: String}, //TODO to be clarfied
+  categoryIds: {type: [mongoose.ObjectId]},
+})
+
+module.exports = mongoose.model('items', itemSchema)
