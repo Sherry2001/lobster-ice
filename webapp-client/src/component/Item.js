a6647b12a07e5c9f136983496abb62ebe4de1b12
Sherry Shi <sherryshi@SherrysMacBook4.home>
2020-06-24 18:20:04 -0400

    components stylesheet folders structure

--- webapp-client/src/component/Item.js
+++ webapp-client/src/component/Item.js
@@ -2,6 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Item.css';
 
-export class Item extends React.Component {
-  static prop
+const Item = (props) => {
+  return (
+    <div onclick="foo"></div>
+  );
+};
+
+function foo() {
+  
 }
+
+export default Item;
