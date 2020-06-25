9e654475243aa690564a2e9e26791d260fee6f64
Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
2020-06-25 13:07:03 -0400

    add actual sidebar injection, load highlighted text

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,6 +1,36 @@
 console.log('here');
+const bulmaLink = document.createElement('link');
+    bulmaLink.setAttribute('href','https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css');
+    bulmaLink.setAttribute('rel','stylesheet');
+    document.head.appendChild(bulmaLink)
+
+    const sidebar = document.createElement('div');
+    sidebar.id = 'lobstericecream';
+    sidebar.classList.add('panel');
+    sidebar.classList.add('injection-panel');
+    sidebar.classList.add('invisible'); 
+    document.body.appendChild(sidebar); 
+
 document.onmouseup = document.onkeyup = document.onselectionchange = function() {
   const content = window.getSelection().toString(); 
   console.log(content);  
   chrome.storage.sync.set({ content });
+  if (sidebar.classList.contains('invisible')) {
+    sidebar.classList.remove('invisible');
+  }
+  sidebar.innerHTML = '';
+  createSidebar(content)
 };
+
+function createSidebar(content) {
+    const panelHeading = document.createElement('div');
+    panelHeading.classList.add('panel-heading');
+    panelHeading.innerText = "Lobster Ice Cream"; 
+    sidebar.appendChild(panelHeading);
+
+    const panelBlock = document.createElement('div');
+    panelBlock.classList.add('panel-block');
+    panelBlock.innerText = "Hello this is a panel block inner text+" + content;
+    sidebar.appendChild(panelBlock);
+}
+    
