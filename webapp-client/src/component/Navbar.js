9d7ffbff76d3425989c355b942a5300a52f3d933
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-06-25 13:58:15 -0700

    navbar with add category feature

--- webapp-client/src/component/Navbar.js
+++ webapp-client/src/component/Navbar.js
@@ -3,18 +3,63 @@ import React from 'react';
 import ReactShadowScroll from 'react-shadow-scroll';
 import '../stylesheets/Navbar.css';
 
-const Navbar = (props) => {
-  return (
-    <ReactShadowScroll>
-      <ul className="nav-bar">
-        <li>All</li>
-        <li>France Trip</li>
-        <li>Senior Trip</li>
-        <li>Foods I want</li>
-        <li>China</li>
-      </ul>
-    </ReactShadowScroll>
-  );
-};
+const Navbar = React.createClass({
+  getInitialState() {
+    return ({
+      categories: {
+        'category-1': 'All'
+      }
+    });
+  },
+  addCategory(category) {
+    const timestamp = (new Date()).getTime();
+    this.state.categories['category' + timestamp] = category;
+    this.setState({categories: this.state.categories});
+  },
+  render() {
+    return (
+      <div className='nav-bar'>
+        <CategoryList categories={this.state.categories} />
+        <AddCategoryForm addCategory={this.addCategory} />
+      </div>
+    )
+  }
+});
+
+const CategoryList = React.createClass({
+  render() {
+    return(
+      <ReactShadowScroll>
+        <ul>
+          {
+            Object.keys(this.props.categories).map(function(key) {
+              return <li className="list-category">{this.props.categories[key]}</li>
+            }.bind(this))
+          }
+        </ul>
+      </ReactShadowScroll>
+    );
+  }
+});
+
+const AddCategoryForm = React.createClass({
+  createCategory() {
+    let category = this.refs.categoryName.value;
+    if(typeof cateogry === 'string' && category.length > 0) {
+      this.props.addCategory(category);
+      this.refs.categoryForm.reset();
+    }
+  },
+  render() {
+    return(
+      <form className="add-category-form" ref="categoryForm" onSubmit={this.createCategory}>
+        <label for="categoryName">
+          Category Name
+        </label>
+        <button type="submit" className="submit-button">Add Category</button>
+      </form>
+    );
+  }
+});
 
 export default Navbar;
