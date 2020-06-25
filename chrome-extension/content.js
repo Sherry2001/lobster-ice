94df3dd7425b1838040f00e8d620546348459353
Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
2020-06-25 19:53:01 -0400

    button shows up whenever new content is highlighted on page

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,32 +1,68 @@
 console.log('here');
 const bulmaLink = document.createElement('link');
-    bulmaLink.setAttribute('href','https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css');
-    bulmaLink.setAttribute('rel','stylesheet');
-    document.head.appendChild(bulmaLink)
+bulmaLink.setAttribute('href','https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css');
+bulmaLink.setAttribute('rel','stylesheet');
+document.head.appendChild(bulmaLink)
 
-    const sidebar = document.createElement('div');
-    sidebar.id = 'lobstericecream';
-    sidebar.classList.add('panel','injection-panel','invisible','px-4','py-4');
-    document.body.appendChild(sidebar); 
+const sidebar = document.createElement('div');
+sidebar.id = 'lobstericecream';
+sidebar.classList.add('panel','injection-panel','px-4','py-4');
+document.body.appendChild(sidebar); 
 
-      
-document.onmouseup = document.onkeyup = document.onselectionchange = function() {
-  const content = window.getSelection().toString(); 
-  console.log(content);  
-  chrome.storage.sync.set({ content });
-  if (sidebar.classList.contains('invisible')) {
-    sidebar.classList.remove('invisible');
-  }
-  sidebar.innerHTML = '';
-  createSidebar(content)
+const iconContainer = document.createElement('div');
+iconContainer.classList.add('injection-icon');
+iconContainer.id = 'icon-container';
+const iconButton = document.createElement('button');
+iconButton.classList.add('button','is-info');
+iconButton.innerHTML = 'Add to Lobster';
+iconContainer.appendChild(iconButton);
+    // const icon = document.createElement('img');
+    // icon.src = 'assets/lobster.jpg';
+    // iconButton.appendChild(icon);
+document.body.appendChild(iconContainer);
+
+let lastContent; 
+
+document.onclick = (event) => {
+    console.log('got in here');
+    console.log(iconContainer.style.display);
+    if (iconContainer.style.display === 'block') {
+        console.log('current block, change to none')
+        iconContainer.style.display ='none'; 
+    } 
+   
+        console.log('currently none');
+        const content = window.getSelection().toString();
+        if (content && content.length > 0 && content !== lastContent) {
+            console.log( 'change to block');
+            showIcon(event, content);
+            lastContent = content;
+            chrome.storage.sync.set({ content });
+
+        }
+        
+        console.log(content); 
+    
 };
 
+
+function showIcon(event, content) {
+    iconButton.onclick = () => createSidebar(content);
+    iconContainer.style.display = 'block';
+    iconContainer.style.left = event.pageX - 50 + 'px';
+    iconContainer.style.top = event.pageY - 55 + 'px';
+}; 
+
 function createSidebar(content) {
+    sidebar.innerHTML = '';
     const panelHeading = document.createElement('div');
     panelHeading.classList.add('panel-heading');
     panelHeading.innerText = 'Lobster Ice Cream'; 
     sidebar.appendChild(panelHeading);
    
+    const sidebarHeader = document.createElement('div');
+    sidebarHeader.classList.add('navbar');
+
     const form = document.createElement('form');
     const highlightLabel = document.createElement('label');
     highlightLabel.classList.add('label','mt-2');
@@ -51,9 +87,5 @@ function createSidebar(content) {
     buttonContainer.appendChild(addButton);
     form.appendChild(buttonContainer);
 
-    
-
     sidebar.appendChild(form);
 }
-
-    
