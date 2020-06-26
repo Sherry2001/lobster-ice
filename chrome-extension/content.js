9959767bdaa72c973ac88c36e3b0f43aaf519bf0
Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
2020-06-26 01:32:14 -0400

    clean up code, add comments

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,17 +1,20 @@
-console.log('here');
+/**
+ * Content script for all urls 
+ */
+
+//Insert bulma stylesheet
 const bulmaLink = document.createElement('link');
 bulmaLink.setAttribute('href','https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css');
 bulmaLink.setAttribute('rel','stylesheet');
 document.head.appendChild(bulmaLink)
 
+//Inject invisible sidebar onto page
 const sidebar = document.createElement('div');
 sidebar.id = 'lobstericecream';
 sidebar.classList.add('panel','injection-panel','px-4','py-4');
 document.body.appendChild(sidebar); 
 
-    
-
-
+//Inject invisible highlight icon onto page
 const iconContainer = document.createElement('div');
 iconContainer.classList.add('injection-icon');
 iconContainer.id = 'icon-container';
@@ -19,96 +22,88 @@ const iconButton = document.createElement('button');
 iconButton.classList.add('button','is-info');
 iconButton.innerHTML = 'Add to Lobster';
 iconContainer.appendChild(iconButton);
-    // const icon = document.createElement('img');
-    // icon.src = 'assets/lobster.jpg';
-    // iconButton.appendChild(icon);
+//TODO: CHANGE BUTTON TO AN ACTUAL ICON!! 
 document.body.appendChild(iconContainer);
 
+//Keep track of last used highlight
 let lastContent; 
 
+//Show extension icon when selecting text on page
 document.onclick = (event) => {
-    console.log('got in here');
-    const dontListen = '.injection-panel, .injection-icon';
-    //if (!event.target.matches(dontListen)) {
-    console.log(iconContainer.style.display);
-    if (iconContainer.style.display === 'block') {
-        console.log('current block, change to none')
-        iconContainer.style.display ='none'; 
-    } 
-   
-        console.log('currently none');
-        const content = window.getSelection().toString();
-        if (content && content.length > 0 && content !== lastContent) {
-            console.log( 'change to block');
-            showIcon(event, content);
-            lastContent = content;
-            chrome.storage.sync.set({ content });
-
-        }
-        
-        console.log(content); 
-   // }
+  const dontListen = '.injection-panel, .injection-icon';
+  console.log(iconContainer.style.display);
+  if (iconContainer.style.display === 'block') {
+    console.log('current block, change to none')
+    iconContainer.style.display ='none'; 
+  } 
+  const content = window.getSelection().toString();
+  if (content && content.length > 0 && content !== lastContent) {
+    showIcon(event, content);
+    lastContent = content;
+    chrome.storage.sync.set({ content });
+  }
 };
 
+//Excute icon from text-select listener, trigger sidebar when clicked
 iconButton.onclick = (event) => {
-    event.stopPropagation(); 
-    iconContainer.style.display = 'none';
-    createSidebar(lastContent);
+  event.stopPropagation(); 
+  iconContainer.style.display = 'none';
+  createSidebar(lastContent);
 }
 
+//Exclude sidebar from text-select listener
 sidebar.onclick = (event) => {
-    event.stopPropagation();
+  event.stopPropagation();
 }
 
 function showIcon(event, content) {
-    iconContainer.style.display = 'block';
-    iconContainer.style.left = event.pageX - 10 + 'px';
-    iconContainer.style.top = event.pageY - 55 + 'px';
+  iconContainer.style.display = 'block';
+  iconContainer.style.left = event.pageX - 10 + 'px';
+  iconContainer.style.top = event.pageY - 55 + 'px';
 }; 
 
 function closeSidebar() {
   sidebar.style.display = 'none';
 }
 
+//Inject sidebar html content
 function createSidebar(content) {
-    console.log('createSidebar called');
-    sidebar.innerHTML = '';
-    sidebar.style.display = 'block';
-    const panelHeading = document.createElement('div');
-    panelHeading.classList.add('message-header');
-    panelHeading.innerText = 'Lobster Ice Cream'; 
+  sidebar.innerHTML = '';
+  sidebar.style.display = 'block';
+  const panelHeading = document.createElement('div');
+  panelHeading.classList.add('message-header');
+  panelHeading.innerText = 'Lobster Ice Cream'; 
+  //TODO: Include icon in panelhead, update looks
 
-    const close = document.createElement('a');
-    close.classList.add('delete');
-    close.onclick = closeSidebar;
-    panelHeading.appendChild(close);
-    sidebar.appendChild(panelHeading);
-    // const sidebarHeader = document.createElement('div');
-    // sidebarHeader.classList.add('navbar');
+  const close = document.createElement('a');
+  close.classList.add('delete');
+  close.onclick = closeSidebar;
+  panelHeading.appendChild(close);
+  sidebar.appendChild(panelHeading);
 
-    const form = document.createElement('form');
-    const highlightLabel = document.createElement('label');
-    highlightLabel.classList.add('label','mt-2');
-    highlightLabel.innerText = 'Highlighted Text'
-    form.appendChild(highlightLabel);
+  const form = document.createElement('form');
+  const highlightLabel = document.createElement('label');
+  highlightLabel.classList.add('label','mt-2');
+  highlightLabel.innerText = 'Highlighted Text'
+  form.appendChild(highlightLabel);
 
-    const highlightTextarea = document.createElement('textarea');
-    highlightTextarea.classList.add('textarea');
-    form.appendChild(highlightTextarea);
-    highlightTextarea.value = content;
+  const highlightTextarea = document.createElement('textarea');
+  highlightTextarea.classList.add('textarea');
+  form.appendChild(highlightTextarea);
+  highlightTextarea.value = content;
 
-    const noteTextarea = document.createElement('textarea');
-    noteTextarea.classList.add('textarea', 'mt-6');
-    noteTextarea.setAttribute('placeholder','Note to self');
-    form.appendChild(noteTextarea);
+  const noteTextarea = document.createElement('textarea');
+  noteTextarea.classList.add('textarea', 'mt-6');
+  noteTextarea.setAttribute('placeholder','Note to self');
+  form.appendChild(noteTextarea);
 
-    const buttonContainer = document.createElement('div');
-    buttonContainer.classList.add('has-text-centered','mt-1');
-    const addButton = document.createElement('button');
-    addButton.classList.add('button', 'is-link')
-    addButton.innerHTML = 'Add Clipping';
-    buttonContainer.appendChild(addButton);
-    form.appendChild(buttonContainer);
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('has-text-centered','mt-1');
+  const addButton = document.createElement('button');
+  addButton.classList.add('button', 'is-link')
+  addButton.innerHTML = 'Add Clipping';
+  buttonContainer.appendChild(addButton);
+  form.appendChild(buttonContainer);
 
-    sidebar.appendChild(form);
+  sidebar.appendChild(form);  
 }
