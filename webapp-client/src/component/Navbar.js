86955f105da357bd63cc649bd2fcef27c9df2a69
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-06-26 10:28:11 -0700

    form added for the add category feature, debugged code that runs now

--- webapp-client/src/component/Navbar.js
+++ webapp-client/src/component/Navbar.js
@@ -3,19 +3,24 @@ import React from 'react';
 import ReactShadowScroll from 'react-shadow-scroll';
 import '../stylesheets/Navbar.css';
 
-const Navbar = React.createClass({
-  getInitialState() {
-    return ({
-      categories: {
-        'category-1': 'All'
+export default class Navbar extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories : {
+        'category-1' : 'All',
+        'category-2' : 'France Trip',
+        'category-3' : 'Senior Trip',
+        'category-4' : 'All the food',
+        'category-5' : 'China Trip'
       }
-    });
-  },
+    };
+  }
   addCategory(category) {
     const timestamp = (new Date()).getTime();
-    this.state.categories['category' + timestamp] = category;
-    this.setState({categories: this.state.categories});
-  },
+    const newCategories = Object.assign({}, this.state.categories, {['category' + timestamp]: category});
+    this.setState({categories: newCategories});
+  }
   render() {
     return (
       <div className='nav-bar'>
@@ -24,42 +29,39 @@ const Navbar = React.createClass({
       </div>
     )
   }
-});
+}
 
-const CategoryList = React.createClass({
-  render() {
-    return(
-      <ReactShadowScroll>
+class CategoryList extends React.Component {
+  render () {
+    return (
+      <ReactShadowScroll className="list-category">
         <ul>
           {
-            Object.keys(this.props.categories).map(function(key) {
-              return <li className="list-category">{this.props.categories[key]}</li>
-            }.bind(this))
+            Object.keys(this.props.categories).map((key) => {
+              return <li>{this.props.categories[key]}</li>
+            })
           }
         </ul>
       </ReactShadowScroll>
-    );
+    )
   }
-});
+}
 
-const AddCategoryForm = React.createClass({
-  createCategory() {
+class AddCategoryForm extends React.Component {
+  createCategory = () => {
     let category = this.refs.categoryName.value;
     if(typeof cateogry === 'string' && category.length > 0) {
       this.props.addCategory(category);
       this.refs.categoryForm.reset();
     }
-  },
-  render() {
+  }
+  render () {
     return(
       <form className="add-category-form" ref="categoryForm" onSubmit={this.createCategory}>
-        <label for="categoryName">
-          Category Name
-        </label>
+        <label htmlFor="categoryName">New Category: </label>
+        <input type="text" name="categoryName"></input><br></br>
         <button type="submit" className="submit-button">Add Category</button>
       </form>
-    );
+    )
   }
-});
-
-export default Navbar;
+}
