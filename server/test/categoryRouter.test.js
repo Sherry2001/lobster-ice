e5d46f477cc3fcadbee29030ff78f39cc519ee6c
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-06-29 02:30:04 -0400

    Add basic testing structure and dependencies for using an in-memory mongodb.

--- server/test/categoryRouter.test.js
+++ server/test/categoryRouter.test.js
@@ -1,2 +1,27 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const MongoClient = require('mongodb');
+const MongoMemoryServer = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+const expect = chai.expect
+
+before(async function () {
+	mongoServer = new MongoMemoryServer.default();
+	const mongoUri = await mongoServer.getUri();
+	await mongoose.connect(mongoUri);
+});
+
+after(async function () {
+	await mongoose.disconnect();
+	await mongoServer.stop();
+});
+
+describe('...', function () {
+	it('...', async () => {
+		const User = mongoose.model('User', new mongoose.Schema({ name: String }));
+		const cnt = await User.count();
+		expect(cnt).to.equal(0);
+	});
+});
