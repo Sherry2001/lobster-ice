34ce23554e359bdef42017d5f0133ef874eff67f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-06-29 03:42:17 -0400

    Add basic example for how to use mongoose with mocha/chai.

--- server/test/categoryRouter.test.js
+++ server/test/categoryRouter.test.js
@@ -1,27 +1,44 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const Category = require("../db/models/category");
 const MongoClient = require('mongodb');
 const MongoMemoryServer = require('mongodb-memory-server');
 const mongoose = require('mongoose');
+const User = require("../db/models/user");
 
+let exampleCategory;
+let defaultUser;
 let mongoServer;
-const expect = chai.expect
+const expect = chai.expect;
+
+function handleErr(err) {
+  if (err) {
+    throw err;
+  }
+}
 
 before(async function () {
-	mongoServer = new MongoMemoryServer.default();
-	const mongoUri = await mongoServer.getUri();
-	await mongoose.connect(mongoUri);
+  mongoServer = new MongoMemoryServer.default();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+  defaultUser = new User({ email: "lobster-ice-cream-lover@gmeil.com" });
+  defaultUser.save(handleErr);
+  exampleCategory = new Category({ title: "hello there", userId: defaultUser._id });
+  exampleCategory.save(handleErr);
 });
 
 after(async function () {
-	await mongoose.disconnect();
-	await mongoServer.stop();
+  await mongoose.disconnect();
+  await mongoServer.stop();
 });
 
-describe('...', function () {
-	it('...', async () => {
-		const User = mongoose.model('User', new mongoose.Schema({ name: String }));
-		const cnt = await User.count();
-		expect(cnt).to.equal(0);
-	});
+describe('categoryRouter', function () {
+  it('...', async function () {
+    await Category.find(function (err, categories) {
+      handleErr(err);
+      console.log(categories);
+    });
+    const cnt = await User.count();
+    expect(cnt).to.equal(1);
+  });
 });
