979eceb5b1fd4ee6a1b92a68f9c02daf095c0041
Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
2020-07-01 17:17:36 -0400

    clear obsolete popup code- unused

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,13 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let form = document.getElementById('itemForm');
-    let highlightArea = document.getElementById('highlight');
-    chrome.storage.sync.get(stored => {
-      console.log(stored.content);
-      highlightArea.value = stored.content
-    });
-    form.addEventListener('submit', function(e){
-      e.preventDefault();
-      let value = e.target.children.highlight.value
-      console.log(value)
-    })
+  //TODO: popup.html action
 }, false);
