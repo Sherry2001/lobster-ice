6a40250e0007799030460e4723a92ba9718af034
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-07-06 10:04:55 -0700

    changes to index and App file so it displays components properly

--- webapp-client/src/App.js
+++ webapp-client/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './lobster-icon.jpg';
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lobster Ice Cream</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Navbar />
     </div>
   );
 }
