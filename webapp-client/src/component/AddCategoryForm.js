fcc01085d300e4b4cd5dc82d908a41614bf2f31d
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-07-08 16:31:06 -0700

    AddCategoryForm component responsible for the taking in inputs to add category and add to the existing list of categories

---
+++ webapp-client/src/component/AddCategoryForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../stylesheets/AddCategoryForm.css';
+
+export default class AddCategoryForm extends React.Component {
+  createCategory = (e) => {
+    e.preventDefault();
+    const category = this._inputElement.value;
+    this.props.addCategory(category);
+    this._inputElement.value = '';
+  };
+  render() {
+    return (
+      <form className="add-category-form" onSubmit={this.createCategory}>
+        <label htmlFor="categoryName">New Category: </label>
+        <input
+          ref={(input) => (this._inputElement = input)}
+          type="text"
+          name="categoryName"
+        ></input>
+        <br></br>
+        <button type="submit" className="submit-button">
+          Add Category
+        </button>
+      </form>
+    );
+  }
+}
