19b2b9197365b623845811a5c1938661855bd07f
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-07-10 13:52:23 -0700

    ErrorMessage component to display error message from server

---
+++ webapp-client/src/component/ErrorMessage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class ErrorMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openWindow: true,
+    }
+    this.displayError = this.displayError.bind(this);
+  }
+
+  displayError() {
+    if (this.props.hasError && this.state.openWindow) {
+      return (
+        <div className="notification is-warning">
+          <button className="delete" onClick={this.props.closePopup}></button>
+          {this.props.message}
+        </div>
+      );
+    }
+    return (
+      <div></div>
+    );
+  }
+
+  render() {
+    return (
+      this.displayError()
+    );
+  }
+}
