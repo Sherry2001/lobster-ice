8925fcf6388fde753ce52b534ea617d2a2f3f515
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-07-10 14:21:03 -0700

    adding label for form, fixing comment style, removing <br> for vertical spacing

--- webapp-client/src/component/AddCategoryForm.js
+++ webapp-client/src/component/AddCategoryForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default class AddCategoryForm extends React.Component {
-  /** This function will be changed later to directly POST request to db instead of calling addCategory */
+  // This function will be changed later to directly POST request to db instead of calling addCategory
   createCategory = (e) => {
     e.preventDefault();
     const category = this._inputElement.value;
@@ -11,16 +11,19 @@ export default class AddCategoryForm extends React.Component {
   render() {
     return (
       <form className="my-1" onSubmit={this.createCategory}>
+        <label className="label" for="add-category">
+          Add Category:
+        </label>
         <input
           ref={(input) => (this._inputElement = input)}
           type="text"
+          name="add-category"
           className="input is-rounded"
           placeholder="New Category"
         ></input>
-        <br></br>
         <div className="has-text-centered my-1">
           <button type="submit" className="button">
-            Add Category
+            Add
           </button>
         </div>
       </form>
