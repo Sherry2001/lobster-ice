3c5a9a34842a01e7eba6b7a4ea34dc1cec2d66eb
Sherry Shi <sherryshi2001@gmail.com>
2020-07-13 14:05:33 -0400

    // space, undo formatter

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -2,8 +2,8 @@
  * Content script for all urls
  */
 
-//Insert bulma stylesheet
-//TODO: STOP USING BULMA ONCE I WRITE MY OWN CSS FOR ALL EXTENSION ELEMENTS
+// Insert bulma stylesheet
+// TODO: STOP USING BULMA ONCE I WRITE MY OWN CSS FOR ALL EXTENSION ELEMENTS
 const bulmaLink = document.createElement('link');
 bulmaLink.setAttribute(
   'href',
@@ -12,46 +12,41 @@ bulmaLink.setAttribute(
 bulmaLink.setAttribute('rel', 'stylesheet');
 document.head.appendChild(bulmaLink);
 
-//Inject invisible sidebar onto page
-const sidebar = customCreateElement('div', [
-  'panel',
-  'injection-sidebar',
-  'px-4',
-  'py-4',
-]);
+// Inject invisible sidebar onto page
+const sidebar = customCreateElement('div', ['panel', 'injection-sidebar', 'px-4', 'py-4']);
 sidebar.id = 'lobstericecream';
 document.body.appendChild(sidebar);
 
-//Inject invisible highlight icon onto page
+// Inject invisible highlight icon onto page
 const iconButton = customCreateElement('button', ['injection-icon'], 'Save');
-//TODO: CHANGE BUTTON TO AN ACTUAL ICON!!
+// TODO: CHANGE BUTTON TO AN ACTUAL ICON!!
 
 document.body.appendChild(iconButton);
 
-//Keep track of last used highlight
+// Keep track of last used highlight
 let lastContent;
 
-//Show extension icon when selecting text on page
+// Show extension icon when selecting text on page
 document.onclick = (event) => {
   if (iconButton.style.display === 'block') {
     iconButton.style.display = 'none';
   }
   const content = window.getSelection().toString();
-  //TODO: Trim Text
+  // TODO: Trim Text
   if (content && content !== lastContent) {
     showIcon(event);
     lastContent = content;
   }
 };
 
-//Exclude icon from text-select listener, trigger sidebar when clicked
+// Exclude icon from text-select listener, trigger sidebar when clicked
 iconButton.onclick = (event) => {
   event.stopPropagation();
   iconButton.style.display = 'none';
   createSidebar(lastContent);
 };
 
-//Exclude sidebar from text-select listener
+// Exclude sidebar from text-select listener
 sidebar.onclick = (event) => {
   event.stopPropagation();
 };
@@ -66,17 +61,13 @@ function closeSidebar() {
   sidebar.style.display = 'none';
 }
 
-//Inject sidebar html content
+// Inject sidebar html content
 function createSidebar(content) {
   sidebar.innerHTML = '';
   sidebar.style.display = 'block';
 
-  const panelHeading = customCreateElement(
-    'div',
-    ['message-header'],
-    'Lobster Ice Cream'
-  );
-  //TODO: Include icon in panelhead, update looks
+  const panelHeading = customCreateElement('div', ['message-header'], 'Lobster Ice Cream');
+  // TODO: Include icon in panelhead, update looks
 
   const close = customCreateElement('a', ['delete']);
   close.onclick = closeSidebar;
@@ -85,11 +76,7 @@ function createSidebar(content) {
 
   const form = document.createElement('form');
 
-  const highlightLabel = customCreateElement(
-    'label',
-    ['label', 'mt-2'],
-    'Highlighted Text'
-  );
+  const highlightLabel = customCreateElement('label', ['label', 'mt-2'], 'Highlighted Text');
   form.appendChild(highlightLabel);
 
   const highlightTextarea = customCreateElement('textarea', ['textarea']);
@@ -100,16 +87,9 @@ function createSidebar(content) {
   commentTextarea.setAttribute('placeholder', 'Note to self');
   form.appendChild(commentTextarea);
 
-  const buttonContainer = customCreateElement('div', [
-    'has-text-centered',
-    'mt-1',
-  ]);
+  const buttonContainer = customCreateElement('div', ['has-text-centered', 'mt-1']);
 
-  const addButton = customCreateElement(
-    'button',
-    ['button', 'is-link'],
-    'Add Clipping'
-  );
+  const addButton = customCreateElement('button', ['button', 'is-link'], 'Add Clipping');
   buttonContainer.appendChild(addButton);
   form.appendChild(buttonContainer);
 
