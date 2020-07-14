18103ac1225d6af92f4befcaac1bb236b08e984f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-07-14 12:53:20 -0400

    Add working example for fetching user items

--- webapp-client/src/component/ContentPane.js
+++ webapp-client/src/component/ContentPane.js
@@ -1,43 +1,48 @@
-import React from "react";
-import Category from "./Category";
+import React from 'react';
+import Category from './Category';
 import Item from './Item';
 import '../stylesheets/ContentPane.css';
 
 export default class ContentPane extends React.Component {
-  constructor(props) {
-    const eiffelTower = {
-      highlight: "eiffel tower",
-      sourceLink: ".",
-    };
+  constructor (props) {
     super(props);
     this.state = {
-      displayCategory: <Category categoryName={'All'} />,
-      items: [
-        eiffelTower,
-        eiffelTower,
-        eiffelTower,
-        eiffelTower,
-      ],
+      displayCategory: <Category categoryName='All' />,
+      items: []
     };
     this.setContentPane = this.setContentPane.bind(this);
   }
 
-  setContentPane(content) {
+  async componentDidMount () {
+    try {
+      const dummyUser = '5f050952f516f3570ee26724';
+      const response = await fetch('http://localhost:8080/item/getItems/' + dummyUser);
+      if (response.status !== 200) {
+        throw new Error(response.statusMessage);
+      }
+      const items = await response.json();
+      this.setState({ items });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  setContentPane (content) {
     this.setState({
       displayCategory: content
     });
   }
 
-  render() {
+  render () {
     return (
-      <React.Fragment>
+      <>
         {this.state.displayCategory}
-        <div className="wrap tile is-ancestor">
+        <div className='wrap tile is-ancestor'>
           {this.state.items.map((item, i) => {
             return <Item key={i} item={item} />;
           })}
         </div>
-      </React.Fragment>
+      </>
     );
   }
 }
