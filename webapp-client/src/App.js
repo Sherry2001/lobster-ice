795418e13a23b81a95a78306dbc82acd992486ce
cynthia-doc <39499154+cynthia-doc@users.noreply.github.com>
2020-07-15 12:29:55 -0700

    Frontend React Navbar (#13)
    * react client app set up
    
    * removed web-client
    
    * remove unnecessary files
    
    * commit folder with item component
    
    * components stylesheet folders structure
    
    * item web layout (not including places api)
    
    * stylesheet for item
    
    * hardcoded navbar
    
    * navbar with add category feature
    
    * form added for the add category feature, debugged code that runs now
    
    * changes to index and App file so it displays components properly
    
    * Category componenet displayed on ContentPane
    
    * navbar with working add category feature
    
    * feature that displays the category component of the category being clicked on
    
    * lobster icon
    
    * add styling for Category component, format files using formatter
    
    * changed height of navbar
    
    * dependencies
    
    * deleting unnecessary comments, changing let to const for constant variables
    
    * deleting unnecessary comment
    
    * new line at the end of the file
    
    * css not applied issue fixed (no longer storing Category components)
    
    * scroll working for navbar, removing react-shadow-scroll
    
    * removing index.css file since components are styled individually
    
    * rearranged devdependencies
    
    * scroll in stylesheet
    
    * remove favicon.ico(react icon)
    
    * website tab displying our website name and icon
    
    * remove importing style
    
    * removing extra line
    
    * remove Navbar component for restructuring
    
    * CategoryList component responsible for rendering list of categories
    
    * AddCategoryForm component responsible for the taking in inputs to add category and add to the existing list of categories
    
    * comment about createCategory function added
    
    * center category name
    
    * updated App component in charge of rendering the overall structure of webapp and storing info about list of categories (hardcoded for now and fetched from db in future version)
    
    * Add support for bulma with the help of @cynthia-doc
    
    * Delete unnecessary files.
    
    * removing setting for control column (now styled with bulma)
    
    * rename image files, call Fragment through React.Fragment
    
    * adding label for form, fixing comment style, removing <br> for vertical spacing
    
    * no longer storing category name since it is passed in as a prop
    
    * remove setupTests.js
    
    * adding line between functions; getting form input without going through ref
    
    * using proptypes to specify props fields, wrap props method inside a bound method
    
    * remove index.css
    
    * rename default to defaultCategory, fixing comment style
    
    * lower casing App-logo to app-logo
    
    * add prop-types as a dependency
    
    * removed irrelevant pointer-even setting
    
    * .env file for global url
    
    * add instruction on how to use the .env variable for server url
    
    * fix wrong port
    
    * remove TODO comment
    
    Co-authored-by: Sherry <sherryshi2001@gmail.com>
    Co-authored-by: Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
    Co-authored-by: Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
    Co-authored-by: Sherry Shi <sherryshi@SherrysMacBook4.home>
    Co-authored-by: Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>

---
+++ webapp-client/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import logo from './lobster-icon.jpg';
+import './App.css';
+import CategoryList from './component/CategoryList';
+import AddCategoryForm from './component/AddCategoryForm';
+import Category from './component/Category';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // hard-coded for now, will be fetched from db
+    this.defaultCategory = 'All';
+    this.state = {
+      // list of category names for now, will be list of category ids later 
+      categories: [this.defaultCategory],
+      currentCategory: this.defaultCategory,
+    };
+    this.setCurrentCategory = this.setCurrentCategory.bind(this);
+    this.addCategory = this.addCategory.bind(this);
+  }
+
+  setCurrentCategory(category) {
+    this.setState({
+      currentCategory: category,
+    });
+  }
+
+  // Called in AddCategoryForm, to be deleted later when fetching from db
+  addCategory(category) {
+    this.setState({
+      categories: [...this.state.categories, category],
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <h1>Lobster Ice Cream</h1>
+          <img src={logo} className="app-logo" alt="logo" />
+        </div>
+        <div className="columns">
+          <div className="column is-one-fifth">
+            <nav className="panel pb-1">
+              <CategoryList
+                categories={this.state.categories}
+                setContentPane={this.setCurrentCategory}
+              />
+              <div className="panel-block"></div>
+              {/* TODO: Pull AddCategoryForm to the bottom of the page */}
+              <AddCategoryForm addCategory={this.addCategory} />
+            </nav>
+          </div>
+          <div className="column">
+            {/* Category will later be replaced by a component named ItemContainer including category name and list of item ids of that category */}
+            <Category categoryName={this.state.currentCategory} />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
