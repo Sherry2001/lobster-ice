1f3423765de87490f15d6d2ae6d23c57e1c151eb
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-07-15 17:12:19 -0400

    Remove unused href.

--- webapp-client/src/component/Navbar.js
+++ webapp-client/src/component/Navbar.js
@@ -10,7 +10,7 @@ class Navbar extends Component {
         aria-label="main navigation"
       >
         <div className="navbar-brand">
-          <a className="navbar-item" href="">
+          <a className="navbar-item">
             <img
               src={logo}
               alt="Lobster Ice Cream Logo (a cute lobster)"
