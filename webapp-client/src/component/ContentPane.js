2761d124637a0f2e0bdef393246188f25a08252b
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-07-17 09:46:24 -0400

    Clean up componentDidMount hook for ContentPane by introducing another method.

--- webapp-client/src/component/ContentPane.js
+++ webapp-client/src/component/ContentPane.js
@@ -15,28 +15,29 @@ export default class ContentPane extends React.Component {
 
   async componentDidMount() {
     const { category, userId } = this.props;
+    const items = await this.getItems(category, userId);
+    this.setState({ items });
+  }
+
+  async getItems(category, userId) {
     let url, response, items;
-    try {
-      if (category === 'All') {
-        url = process.env.REACT_APP_API_URL + '/item/getItems/' + userId;
-        response = await fetch(url);
-        items = await response.json();
-      } else {
-        url =
-          process.env.REACT_APP_API_URL +
-          '/category/getCategoryItems/' +
-          category;
-        response = await fetch(url);
-        const categoryObject = await response.json();
-        items = categoryObject.items;
-      }
-      if (response.status !== 200) {
-        throw new Error(response.statusMessage);
-      }
-      this.setState({ items });
-    } catch (error) {
-      console.error(error);
+    if (category === 'All') {
+      url = process.env.REACT_APP_API_URL + '/item/getItems/' + userId;
+      response = await fetch(url);
+      items = await response.json();
+    } else {
+      url =
+        process.env.REACT_APP_API_URL +
+        '/category/getCategoryItems/' +
+        category;
+      response = await fetch(url);
+      const categoryObject = await response.json();
+      items = categoryObject.items;
     }
+    if (response.status !== 200) {
+      throw new Error(response.statusMessage);
+    }
+    return items;
   }
 
   setContentPane(content) {
