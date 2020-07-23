3c5b6c6ba7c18b5f0b5ea1d543f67d38fdd14a74
cynthia-doc <39499154+cynthia-doc@users.noreply.github.com>
2020-07-23 14:15:51 -0700

    React post create category & error message component (#20)
    * react client app set up
    
    * removed web-client
    
    * remove unnecessary files
    
    * commit folder with item component
    
    * components stylesheet folders structure
    
    * item web layout (not including places api)
    
    * stylesheet for item
    
    * hardcoded navbar
    
    * navbar with add category feature
    
    * form added for the add category feature, debugged code that runs now
    
    * changes to index and App file so it displays components properly
    
    * Category componenet displayed on ContentPane
    
    * navbar with working add category feature
    
    * feature that displays the category component of the category being clicked on
    
    * lobster icon
    
    * add styling for Category component, format files using formatter
    
    * changed height of navbar
    
    * dependencies
    
    * deleting unnecessary comments, changing let to const for constant variables
    
    * deleting unnecessary comment
    
    * new line at the end of the file
    
    * css not applied issue fixed (no longer storing Category components)
    
    * scroll working for navbar, removing react-shadow-scroll
    
    * removing index.css file since components are styled individually
    
    * rearranged devdependencies
    
    * scroll in stylesheet
    
    * remove favicon.ico(react icon)
    
    * website tab displying our website name and icon
    
    * remove importing style
    
    * removing extra line
    
    * remove Navbar component for restructuring
    
    * CategoryList component responsible for rendering list of categories
    
    * AddCategoryForm component responsible for the taking in inputs to add category and add to the existing list of categories
    
    * comment about createCategory function added
    
    * center category name
    
    * updated App component in charge of rendering the overall structure of webapp and storing info about list of categories (hardcoded for now and fetched from db in future version)
    
    * Add support for bulma with the help of @cynthia-doc
    
    * Delete unnecessary files.
    
    * removing setting for control column (now styled with bulma)
    
    * addCategoryForm posts to db successfully (help of Anthony & Sherry for debugging:))
    
    * ErrorMessage component to display error message from server
    
    * rename image files, call Fragment through React.Fragment
    
    * adding label for form, fixing comment style, removing <br> for vertical spacing
    
    * no longer storing category name since it is passed in as a prop
    
    * remove setupTests.js
    
    * adding line between functions; getting form input without going through ref
    
    * using proptypes to specify props fields, wrap props method inside a bound method
    
    * remove index.css
    
    * rename default to defaultCategory, fixing comment style
    
    * lower casing App-logo to app-logo
    
    * add prop-types as a dependency
    
    * change location of where form input is cleared
    
    * removed irrelevant pointer-even setting
    
    * .env file for global url
    
    * add instruction on how to use the .env variable for server url
    
    * fix wrong port
    
    * use .env for server url
    
    * remove TODO comment
    
    * remove call to add category (since react-get-categorylist will be merged into master and removes this necessity)
    
    * removing empty item array request
    
    * Remove debugging lines
    
    * use testing user id
    
    * fix style and convention
    
    * use errorify (not in this PR)
    
    Co-authored-by: Sherry <sherryshi2001@gmail.com>
    Co-authored-by: Sherry Shi <sherryshi@Sherrys-MacBook-4.local>
    Co-authored-by: Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
    Co-authored-by: Sherry Shi <sherryshi@SherrysMacBook4.home>
    Co-authored-by: Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>

--- webapp-client/src/component/ErrorMessage.js
+++ webapp-client/src/component/ErrorMessage.js
@@ -1,14 +1,27 @@
 import React from 'react';
+<<<<<<< HEAD
 import PropTypes from 'prop-types';
+=======
+>>>>>>> React post create category & error message component (#20)
 
 export default class ErrorMessage extends React.Component {
   constructor(props) {
     super(props);
+<<<<<<< HEAD
+=======
+    this.state = {
+      openWindow: true,
+    };
+>>>>>>> React post create category & error message component (#20)
     this.displayError = this.displayError.bind(this);
   }
 
   displayError() {
+<<<<<<< HEAD
     if (this.props.hasError) {
+=======
+    if (this.props.hasError && this.state.openWindow) {
+>>>>>>> React post create category & error message component (#20)
       return (
         <div className="notification is-warning">
           <button className="delete" onClick={this.props.closePopup}></button>
@@ -23,9 +36,12 @@ export default class ErrorMessage extends React.Component {
     return this.displayError();
   }
 }
+<<<<<<< HEAD
 
 ErrorMessage.propTypes = {
   closePopup: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired,
   hasError: PropTypes.bool,
 };
+=======
+>>>>>>> React post create category & error message component (#20)
