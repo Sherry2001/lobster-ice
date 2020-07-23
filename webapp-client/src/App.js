b7484c1f0db35cec84bf6a6f85c9771460b5b0b0
Cynthia Hu <cynthiahu@Cynthias-MacBook-Pro.local>
2020-07-23 16:09:33 -0700

    use errorify, change active tab to different background

--- webapp-client/src/App.js
+++ webapp-client/src/App.js
@@ -34,6 +34,7 @@ export default class App extends React.Component {
           <div className="column is-one-fifth">
             <nav className="panel pb-1">
               <CategoryList
+                currentCategoryId={this.state.categoryId}
                 setCurrentCategory={this.setCurrentCategory}
                 //TODO: Take in userID from OAuth */
                 userID="5f050952f516f3570ee26724"
